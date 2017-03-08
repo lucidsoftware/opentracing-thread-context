aef99b81820cf910189cf3ff180bf4075fd49ac6
Paul Draper <paul@lucidchart.com>
2017-03-08 08:13:49 -0700

    thread-context 0.7

--- thread-context/build.sbt
+++ thread-context/build.sbt
@@ -1,5 +1,5 @@
 libraryDependencies ++= Seq(
-  "com.lucidchart" % "thread-context" % "0.5",
+  "com.lucidchart" % "thread-context" % "0.7",
   "io.opentracing" % "opentracing-api" % "0.20.7",
   "io.opentracing" % "opentracing-noop" % "0.20.7"
 )
---
+++ thread-context/src/main/java/io/opentracing/threadcontext/ContextSpan.java
@@ -0,0 +1,37 @@
+package io.opentracing.threadcontext;
+
+import com.github.threadcontext.Context;
+import com.github.threadcontext.MutableContextSupplier;
+import com.github.threadcontext.ThreadLocalContextSupplier;
+import com.github.threadcontext.control.TryFinallyContext;
+import io.opentracing.NoopSpan;
+import io.opentracing.Span;
+
+public class ContextSpan {
+
+    public static final ContextSpan DEFAULT = new ContextSpan(Context.DEFAULT);
+
+    private final ThreadLocal<Span> span;
+
+    public ContextSpan(MutableContextSupplier contextSupplier) {
+        span = new ThreadLocal<Span>() {
+            protected Span initialValue() {
+                return NoopSpan.INSTANCE;
+            }
+        };
+        contextSupplier.suppliers.add(new ThreadLocalContextSupplier<>(span));
+    }
+
+    public Span get() {
+        return span.get();
+    }
+
+    public Context set(Span span) {
+        return new TryFinallyContext(() -> {
+            Span oldValue = this.span.get();
+            this.span.set(span);
+            return () -> this.span.set(oldValue);
+        });
+    }
+
+}
