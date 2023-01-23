58afe1eb5a1a7197274a2c3e0795934ea741e5d7
Sriraam AS <sriraam@lucidchart.com>
2023-01-23 18:34:27 -0500

    added github-actions ci instead of travis

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "$PGP_SECRET" | base64 --decode | gpg --import
+if [[ $GITHUB_REF == refs/tags/* ]]; then
+  command="; publishSigned; sonatypeRelease"
+else
+  command="publishSigned"
+fi
+echo "Running: sbt \"$command\""
+exec sbt "$command"
